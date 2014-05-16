e721c2c88f38b10616d3be036a8d5fe761f6e0ec
Yoshinari Nomura <nom@quickhack.net>
2014-05-16 17:16:34 +0900

    Set env MHC_TZINFO before test

--- spec/mhc_spec.rb
+++ spec/mhc_spec.rb
@@ -14,7 +14,8 @@ end
 
 describe Mhc::Event do
   before :all do
-    RiCal::PropertyValue::DateTime.default_tzid = 'UTC'
+    ENV["MHC_TZINFO"] = 'UTC'
+    RiCal::PropertyValue::DateTime.default_tzid = ENV["MHC_TZINFO"]
   end
 
   before :each do
