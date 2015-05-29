bac4c922bc1b565d8a081bd86c083d8dd35708c7
Yoshinari Nomura <nom@quickhack.net>
2015-05-29 15:46:56 +0900

    Monkey patch to original RiCal
    To utilize the original RiCal https://github.com/rubyredrick/ri_cal,
    introduce this patch from:
      git clone https://github.com/yoshinari-nomura/ri_cal.git
      git diff 369a4ee..dc740e7

--- lib/mhc.rb
+++ lib/mhc.rb
@@ -2,6 +2,65 @@ require 'tzinfo'
 require 'ri_cal'
 require "kconv"
 
+## Monkey patch to the original RiCal https://github.com/rubyredrick/ri_cal
+## delived from:
+##   git clone https://github.com/yoshinari-nomura/ri_cal.git
+##   git diff 369a4ee..dc740e7
+##
+module RiCal
+  class Component #:nodoc:
+
+    def method_missing(selector, *args, &b) #:nodoc:
+      xprop_candidate = selector.to_s
+      if (match = /^(x_[^=]+)(=?)$/.match(xprop_candidate))
+        x_property_key = match[1].gsub('_','-').upcase
+        if match[2] == "="
+          args.each do |val|
+            add_x_property(x_property_key, val)
+          end
+        else
+          x_properties[x_property_key].map {|property| property.ruby_value}
+        end
+      else
+        super
+      end
+    end # def method_missing
+
+    def export_x_properties_to(export_stream) #:nodoc:
+      x_properties.each do |name, props|
+        props.each do | prop |
+          export_stream.puts("#{name}#{prop.to_s}")
+        end
+      end
+    end # def export_x_properties_to
+
+  end # class Component
+
+  class PropertyValue
+    class OccurrenceList < Array
+
+      def visible_params # :nodoc:
+        result = params.dup
+
+        case @elements.first
+        when Date
+          result = {"VALUE" => "DATE"}.merge(params)
+        when DateTime
+          result = {"VALUE" => "DATE-TIME"}.merge(params)
+        end
+
+        if has_local_timezone?
+          result['TZID'] = tzid
+        else
+          result.delete('TZID')
+        end
+        result
+      end
+    end
+  end # class PropertyValue
+
+end # module RiCal
+
 module Mhc # :nodoc:
   def self.default_tzid=(tzid)
     @tzid = tzid
