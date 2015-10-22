5b48eb4c86db934900ba61917e87f3ef87d8bd67
Yoshinari Nomura <nom@quickhack.net>
2015-10-22 21:07:26 +0900

    JSON formatter to add css class name instead of color name

--- lib/mhc/formatter.rb
+++ lib/mhc/formatter.rb
@@ -316,20 +316,17 @@ module Mhc
       def format_body(context)
         events = []
         @occurrences.each do |oc|
-          if oc.categories.map{|c| c.to_s.downcase}.include?('holiday')
-            color = "red"
-          elsif oc.allday?
-            color = "green"
-          else
-            color = ""
-          end
+          class_name = []
+          class_name += oc.categories.map{|c| "mhc-category-#{c.to_s.downcase}"}
+          class_name << (oc.allday? ? "mhc-allday" : "mhc-time-range")
+
           events << {
             id:    oc.record_id,
             allDay: oc.allday?,
             title: oc.subject,
             start: oc.dtstart.iso8601,
             end:   oc.dtend.iso8601,
-            color: color
+            className: class_name
           }
         end
         return events.to_json
