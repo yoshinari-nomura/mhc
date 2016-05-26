f00e82f47deac8fcc66f873102ac8867a1aa7bb6
Yoshinari Nomura <nom@quickhack.net>
2016-05-26 16:17:35 +0900

    Rename variables for better readability

--- lib/mhc/property_value/list.rb
+++ lib/mhc/property_value/list.rb
@@ -6,35 +6,36 @@ module Mhc
       ITEM_SEPARATOR = " "
 
       def initialize(item_class)
-        @list = []
+        @value = []
         @item_class = item_class
       end
 
       def each
-        @list.each do |value|
+        @value.each do |value|
           yield value
         end
       end
 
       def include?(o)
-        @list.include?(o)
+        @value.include?(o)
       end
 
       def empty?
-        @list.empty?
+        @value.empty?
       end
 
       def parse(string)
         string.strip.split(ITEM_SEPARATOR).each do |str|
           item = @item_class.parse(str)
-          @list << item if item
+          @value << item if item
         end
         return self
       end
 
       def to_mhc_string
-        @list.map{|item| item.to_mhc_string}.join(ITEM_SEPARATOR)
+        @value.map{|item| item.to_mhc_string}.join(ITEM_SEPARATOR)
       end
+      alias_method :to_s, :to_mhc_string
 
     end # class List
   end # module PropertyValue
