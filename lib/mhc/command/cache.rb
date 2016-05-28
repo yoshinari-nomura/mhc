1fa4a1bdfb4700fd710fcb3ddb29e17acce63b62
Yoshinari Nomura <nom@quickhack.net>
2016-05-28 20:23:02 +0900

    Dump calendar cache for debug

--- lib/mhc/command/cache.rb
+++ lib/mhc/command/cache.rb
@@ -2,10 +2,11 @@ module Mhc
   module Command
     class Cache
 
-      def initialize(calendar)
-        calendar.events.each do |event|
-          range = event.range
-          puts "#{range.min.to_mhc_string},#{range.max.to_mhc_string},#{event.uid},#{event.subject}"
+      def initialize(datastore)
+        epoch = Date.new(1970, 1, 1)
+        puts"UID,MTIME,MIN,MAX,CATEGORIES,RECURRENCE,SUBJECT"
+        datastore.each_cache_entry do |uid, ent|
+          puts"#{ent.uid},#{ent.mtime},#{epoch + ent.range.min},#{epoch + ent.range.max},#{(ent.categories||[]).join(' ')},#{ent.recurrence},#{ent.subject}"
         end
       end
 
